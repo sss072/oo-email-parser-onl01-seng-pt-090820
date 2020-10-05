b7d116f95d2bbd359914c3f136d397a35f7c1c59
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-10-05 21:19:24 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,5 +1,6 @@
 class EmailAddressParser 
   attr_accessor :addresses_given
   def initialize(addresses_given)
-    @addresses_given = addresses_given.split()
+    @addresses_given = addresses_given.split(/[\s,]/)
+    
     
